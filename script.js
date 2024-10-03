db8fd18047117486bd8b326aba601e386cfc84ab
Anthonyp0809 <evapacheco20@yahoo.com>
2024-10-03 14:20:43 -0700

    fixed merge

--- script.js
+++ script.js
@@ -8,6 +8,18 @@
 	$("#sh").click(function(){
 		$("#s").show();
 	});
+	$("#by").click(function(){
+		$("#Ad").toggle();
+	});
+	$(".incorrect").click(function(){
+		$(this).css("color", "red");
+	});
+	$(".correct").click(function(){
+		$(this).css("color", "green");
+	});
+	$("#ll").click(function(){
+		$("#re").toggle();
+	});
 }); 
 
 
