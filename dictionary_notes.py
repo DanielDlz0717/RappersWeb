0cb6a35dfea1f36ac59c653e6cd8ea20fdb2f101
Anthonyp0809 <evapacheco20@yahoo.com>
2024-10-03 13:50:42 -0700

    anthony

---
+++ dictionary_notes.py
@@ -0,0 +1,20 @@
+student1 = {
+"name": "Alex",
+"birthday": "January 1, 2010",
+"grade":9
+}
+
+print(student1)
+print(studetn1["name"])
+
+if student11["name"] == "Anthony":
+    print ("we have the same name")
+else:
+     print("hi, " + student1 ["name"])
+     
+     student1["grade"] = 10
+     print(student1)
+     
+     
+     student1["favorite color"] = "green"
+     print (      Student1)
--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+ $(document).ready(function(){
+ $("#show").click(function(){
+    $("#g").show();
+  });
+}); 
 
+$(document).ready(function(){
+ $("#ge").click(function(){
+    $("#g2").show();
+  });
+}); 
+
+$(document).ready(function(){
+ $("#sh").click(function(){
+    $("#s").show();
+  });
+}); 
